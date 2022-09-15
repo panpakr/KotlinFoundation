bbf1658f073671d4c5501727b22d840ae1d2b1c5
panpakr <panpakr@gmail.com>
2022-09-16 01:45:42 +0530

    Properties Usage (Late Assignment)

--- src/main/kotlin/basics/Basics.kt
+++ src/main/kotlin/basics/Basics.kt
@@ -18,26 +18,31 @@ fun testPrintFunction() {
 
 fun testProperties() {
 
-        val input = 1    // ==> val input :Int = 1
-
-       // val input3 : Int
-
-       // println("Value of input : $input3")
-       // input3 = 19
-
-
-        //input3 = 29
-        //input = 10
-
+        //Immutable Property : Value
+        val input = 1    //==> val input :Int = 1
+        // input = 10     //==> Val cannot be reassigned -> Compilation Error
         println("Value of input : $input")
 
+        //Mutable Property : Variable
         var input2 = "test"
         input2 = " me "
 
+        println("Value of input : ${input2.uppercase()}")
+
+        //Value usage late assignment
+        val input3 : Int
+       // println("Value of input : $input3")    //==>  Variable 'input3' must be initialized -> Compilation Error
+        input3 = 19
+
+        println("Value of input : $input3")
+
+        //input3 = 29  //==> Val cannot be reassigned -> Compilation Error
+
+        //Variable usage late assignment
         var input4 : String
         input4 = "te"
         println("Value of input : $input4")
         input4 = "hi"
+        println("Value of input : $input4")
 
-        println("Value of input : ${input2.toUpperCase()}")
 }
